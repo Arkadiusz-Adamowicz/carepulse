7bd5728cb24ff403ec480a599fe183b62722f6bc
Arkadiusz Adamowicz <arek.adamowicz81@gmail.com>
2024-07-09 05:58:51 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,30 @@
-import PatientForm from "@/components/forms/PatientForm";
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+import PatientForm from '@/components/forms/PatientForm'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
 
 const Home = () => {
   return (
-    <div className="flex h-screen mx-h-screen">
+    <div className='mx-h-screen flex h-screen'>
       {/* TODO: OTP Verification */}
 
-      <section className="remove-scrollbar container my-auto">
-        <div className="sub-container max-w-[496px]">
+      <section className='remove-scrollbar container my-auto'>
+        <div className='sub-container max-w-[496px]'>
           <Image
-            src="/assets/icons/logo-full.svg"
+            src='/assets/icons/logo-full.svg'
             width={1000}
             height={1000}
-            alt="patient"
-            className="mb-12 h-10 w-fit"
+            alt='patient'
+            className='mb-12 h-10 w-fit'
           />
 
           <PatientForm />
 
-          <div className="text-14-regular mt-20 flex justify-between">
-            <p className="justify-items-end text-dark-600 xl:text-left">
+          <div className='text-14-regular mt-20 flex justify-between'>
+            <p className='justify-items-end text-dark-600 xl:text-left'>
               © 2024 CarePulse
             </p>
-            <Link href="/?admin=true" className="text-green-500">
+            <Link href='/?admin=true' className='text-green-500'>
               Admin
             </Link>
           </div>
@@ -32,14 +32,14 @@ const Home = () => {
       </section>
 
       <Image
-        src="/assets/images/onboarding-img.png"
+        src='/assets/images/onboarding-img.png'
         width={1000}
         height={1000}
-        alt="patient"
-        className="side-img max-w-[50%]"
+        alt='patient'
+        className='side-img max-w-[50%]'
       />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -1,27 +1,27 @@
-import { Button } from "./ui/button";
-import Image from "next/image";
+import { Button } from './ui/button'
+import Image from 'next/image'
 
 interface ButtonProps {
-  isLoading: boolean;
-  className?: string;
-  children: React.ReactNode;
+  isLoading: boolean
+  className?: string
+  children: React.ReactNode
 }
 
 const SubmitButton = ({ isLoading, className, children }: ButtonProps) => {
   return (
     <Button
-      type="submit"
+      type='submit'
       disabled={isLoading}
-      className={className ?? "shad-primary-btn w-full"}
+      className={className ?? 'shad-primary-btn w-full'}
     >
       {isLoading ? (
-        <div className="flex items-center gap-4">
+        <div className='flex items-center gap-4'>
           <Image
-            src="/assets/icons/loader.svg"
-            alt="loader"
+            src='/assets/icons/loader.svg'
+            alt='loader'
             width={24}
             height={24}
-            className="animate-spin"
+            className='animate-spin'
           />
           Loading...
         </div>
@@ -29,7 +29,7 @@ const SubmitButton = ({ isLoading, className, children }: ButtonProps) => {
         children
       )}
     </Button>
-  );
-};
+  )
+}
 
-export default SubmitButton;
+export default SubmitButton
